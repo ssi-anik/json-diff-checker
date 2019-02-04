bf9b8d835b7ba597714155cd85d55c04d2172dc3
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-02-04 22:33:32 +0600

    data type assumption logic changed

--- src/common.js
+++ src/common.js
@@ -1,4 +1,7 @@
 export function guess_data_type (value) {
+    // Updates on actual answers:
+    // https://stackoverflow.com/a/16776395/2190689
+    // https://stackoverflow.com/a/24629135/2190689
 
     if ( null === value ) {
         return 'Null';
@@ -21,22 +24,34 @@ export function guess_data_type (value) {
     }
 
     var nan = isNaN(Number(value));
-    var isfloat = /^\d*(\.|,)\d*$/;
+    var isFloat = /^\d*(\.|,)\d*$/;
     var commaFloat = /^(\d{0,3}(,)?)+\.\d*$/;
     var dotFloat = /^(\d{0,3}(\.)?)+,\d*$/;
-    var date = /^\d{0,4}(\.|\/|-)\d{0,4}(\.|\/|-)\d{0,4}$/;
+    var date = /^(([0-9]{4}[-/.](0[1-9]|1[0-2])[-/.]([0-2][0-9]|3[0-1])|([0-2][0-9]|3[0-1])[-/.](0[1-9]|1[0-2])[-/.][0-9]{4}))$/;
+    var email = /^[A-za-z0-9._-]*@[A-za-z0-9_-]*\.[A-Za-z0-9.]*$/;
+
+    if ( typeof value === 'string' ) {
+        if ( date.test(value) ) {
+            return "Date";
+        } else if ( email.test(value) ) {
+            return "Email";
+        }
+
+        return 'String';
+    }
 
     if ( !nan ) {
+        // comma separated float value won't reach here.
+        // it's already assumed as STRING.
         if ( parseFloat(value) === parseInt(value) ) {
-            return "Integer";
+            return 'Integer';
         } else {
-            return "Float";
+            return 'Float';
         }
-    } else if ( isfloat.test(value) || commaFloat.test(value) || dotFloat.test(value) ) {
-        return "Float";
-    } else if ( date.test(value) ) {
-        return "Date";
+    } else if ( isFloat.test(value) || commaFloat.test(value) || dotFloat.test(value) ) {
+        // shouldn't even reach here as well
+        return 'Float';
     } else {
-        return "String";
+        return 'String';
     }
 }
